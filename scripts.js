66eba837fe875d1892e9cd2e332219e85c919b05
camilaek <121921741+camilaek@users.noreply.github.com>
2023-01-04 21:56:15 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*
+function siguiente(img)
+{
+    
+    console.log(img);
+    var visor = document.getElementById("visor");
+    var tmp_img = document.getElementById(img);
+
+    visor.src = tmp_img.getAttribute("src");
+}*/
